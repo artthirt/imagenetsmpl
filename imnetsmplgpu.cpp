e2712177f607509a2965483595367a64fa4a72b7
artthirt <artthirt@gmail.com>
2017-09-08 07:18:36 +0300

    changed model

--- imnetsmplgpu.cpp
+++ imnetsmplgpu.cpp
@@ -37,13 +37,13 @@ void ImNetSmplGpu::init()
 
 	m_conv.resize(cnv_size);
 
-	m_conv[0].init(ct::Size(W, H), 3, 3, 64, ct::Size(5, 5), gpumat::LEAKYRELU, false, true, false);
-	m_conv[1].init(m_conv[0].szOut(), 64, 1, 128, ct::Size(3, 3), gpumat::LEAKYRELU, true, true, true);
-	m_conv[2].init(m_conv[1].szOut(), 128, 1, 128, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
-	m_conv[3].init(m_conv[2].szOut(), 128, 1, 256, ct::Size(3, 3), gpumat::LEAKYRELU, true, true, true);
+	m_conv[0].init(ct::Size(W, H), 3, 2, 64, ct::Size(3, 3), gpumat::LEAKYRELU, false, false, false);
+	m_conv[1].init(m_conv[0].szOut(), 64, 2, 64, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
+	m_conv[2].init(m_conv[1].szOut(), 64, 2, 128, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
+	m_conv[3].init(m_conv[2].szOut(), 128, 2, 256, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
 	m_conv[4].init(m_conv[3].szOut(), 256, 1, 256, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
-	m_conv[5].init(m_conv[4].szOut(), 256, 1, 512, ct::Size(3, 3), gpumat::LEAKYRELU, true, true, true);
-	m_conv[6].init(m_conv[5].szOut(), 512, 1, 512, ct::Size(3, 3), gpumat::LEAKYRELU, true, true, true);
+	m_conv[5].init(m_conv[4].szOut(), 256, 2, 512, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
+	m_conv[6].init(m_conv[5].szOut(), 512, 2, 512, ct::Size(3, 3), gpumat::LEAKYRELU, false, true, true);
 
 //	printf("Out=[%dx%dx%d]\n", m_conv.back().szOut().width, m_conv.back().szOut().height, m_conv.back().K);
 
