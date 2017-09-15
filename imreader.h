b815583ab1cdb260c86f2a32367952e46d8332f1
Zaharov <211_Zaharov@lutch.ru>
2017-09-15 09:13:04 +0300

    fix const

--- imreader.h
+++ imreader.h
@@ -59,7 +59,7 @@ public:
 	void get_batch(std::vector< ct::Matf >& X, ct::Matf& y, int batch, bool flip = false, bool aug = false);
 
 	ct::Matf get_image(const std::string& name, bool flip = false, bool aug = false, bool gray = false,
-					   ct::Vec3f &lvls = ct::Vec3f(1, 1, 1), const Point& off = Point());
+					   const ct::Vec3f &lvls = ct::Vec3f(1, 1, 1), const Point& off = Point());
 
 	void getMat(const ct::Matf &in, cv::Mat *out, const ct::Size sz);
 
