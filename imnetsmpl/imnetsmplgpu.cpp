0a45d50a3d36b6ca6b5a15198cc6337c81656ad1
catalips <artthirt@gmail.com>
2018-04-27 06:53:47 +0300

    changed model

--- imnetsmpl/imnetsmplgpu.cpp
+++ imnetsmpl/imnetsmplgpu.cpp
@@ -126,8 +126,8 @@ void ImNetSmplGpu::init()
 
 	m_conv.resize(cnv_size);
 
-    m_conv[0].init(ct::Size(W, H), 3, 2, 64, ct::Size(5, 5), gpumat::LEAKYRELU, false, false, false, true);
-    m_conv[1].init(m_conv[0].szOut(), 64, 2, 128, ct::Size(3, 3), gpumat::LEAKYRELU, false, false, true, true);
+    m_conv[0].init(ct::Size(W, H), 3, 2, 64, ct::Size(7, 7), gpumat::LEAKYRELU, false, false, false, true);
+    m_conv[1].init(m_conv[0].szOut(), 64, 2, 128, ct::Size(5, 5), gpumat::LEAKYRELU, false, false, true, true);
     m_conv[2].init(m_conv[1].szOut(), 128, 2, 256, ct::Size(3, 3), gpumat::LEAKYRELU, false, false, true, true);
     m_conv[3].init(m_conv[2].szOut(), 256, 2, 512, ct::Size(3, 3), gpumat::LEAKYRELU, false, false, true, true);
     m_conv[4].init(m_conv[3].szOut(), 512, 1, 512, ct::Size(3, 3), gpumat::LEAKYRELU, true, false, true, true);
@@ -167,8 +167,8 @@ void ImNetSmplGpu::init()
 		m_conv[i].setDropout(0.7);
 	}
 
-    m_mlp[0].setDropout(.7);
-    m_mlp[1].setDropout(.7);
+    m_mlp[0].setDropout(.94);
+    m_mlp[1].setDropout(.94);
 	m_mlp[2].setDropout(1.);
 
 	m_init = true;
